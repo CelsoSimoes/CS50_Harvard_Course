394fee9c9e5d01e2dba594d824a85613ed778a8a
CelsoSimoes <48064998+CelsoSimoes@users.noreply.github.com>
2020-04-20 16:18:11 -0300

    Aramazendando dados em uma sessao da aplicacao web

--- Aula_2_Flask/app.py
+++ Aula_2_Flask/app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request, session
-#from flask_session import Session
+from flask_session.__init__ import Session #Necessario a instalacao do flask session: 'pip install flask_session'
 
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-#Session(app)
+Session(app)
 
-notes = [] #Defino uma lista vazia que ira salvar minhas 'notas'
+# notes = [] #Defino uma lista vazia que ira salvar minhas 'notas'. Essa e uma variavel global acessivel por toda a aplicacao, nao e o ideal
 
 @app.route("/", methods=["GET", "POST"])
 def notas():
-#    if session.get("notes") is None:
-    #    session["notes"] = []
+    if session.get("notes") is None:
+        session["notes"] = [] #Crio a lista de notas unica da minha sessao
     if request.method == "POST": #Se o metodo request for POST
         note = request.form.get("note") #Acessamos a variavel note
-        notes.append(note) #Acrescento uma nota ao 'notes'
+        session["notes"].append(note) #Acrescento uma nota a minha lista
 
-    return render_template("notes.html", notes=notes)
+    return render_template("notes.html", notes=session["notes"])
