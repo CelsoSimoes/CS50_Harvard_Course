31feefb5b0d4b99a7c75e7ea856d960c1b8f44c8
CelsoSimoes <48064998+CelsoSimoes@users.noreply.github.com>
2020-04-20 15:46:27 -0300

    Armazenando dados no servidor da maquina, em uma lista

--- Aula_2_Flask/aplicacao_sessoes.py
+++ Aula_2_Flask/aplicacao_sessoes.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request, session
-#from flask_session import Session
+from flask_session import Session
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-#Session(app)
+app.config= ["SESSION_PERMANENT"] = False
+app.config= ["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 notes = [] #Defino uma lista vazia que ira salvar minhas 'notas'
 
 @app.route("/", methods=["GET", "POST"])
-def notas():
+def index():
 #    if session.get("notes") is None:
     #    session["notes"] = []
     if request.method == "POST": #Se o metodo request for POST
         note = request.form.get("note") #Acessamos a variavel note
         notes.append(note) #Acrescento uma nota ao 'notes'
 
-    return render_template("notes.html", notes=notes)
+    return render_template("index.html", notes=notes)
