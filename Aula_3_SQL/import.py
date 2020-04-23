6c6d847a6ba29452c0010b312bfb8b19c38038a6
CelsoSimoes <48064998+CelsoSimoes@users.noreply.github.com>
2020-04-23 15:18:02 -0300

    Importando arquivo csvI
    Importando dados do arquivo 'voos.csv', e os adicionando a minha tabela 'voos'

--- Aula_3_SQL/import.py
+++ Aula_3_SQL/import.py
@@ -1,3 +1,4 @@
+import csv #Importa a biblioteca necessaria para fazer a leitura de arquivos '.csv' em Python
 import os #Importa a biblioteca de um sistema operacional
 
 from sqlalchemy import create_engine #
@@ -9,3 +10,10 @@ db = scoped_session(sessionmaker(bind=engine)) #Cria uma sessao encapsulada que 
 
 #OBS: Precisarei realizar importacoes para trabalhar com o Porjeto 1
 def main():
+  f = open("voos.csv") #Variavel que vai armazenar todo meu arquivo csv
+  reader = csv.reader(f) #ferramenta para tonar legivel, ler o arquivo csv
+  for origin, destination, duration in reader: # loop que dá a cada coluna um nome
+      db.execute("INSERT INTO voos (origin, destination, duration) VALUES (:origin, :destination, :duration)", # em verde tenho os 'Placeholders' que uso logo abaixo para atribuir os valores das variavels na leitura do arquivo
+                  {"origin": origin, "destination": destination, "duration": duration}) # Substitui os valores CSV para um SQL
+      print(f"Added flight from {origin} to {destination} lasting {duration} minutes.") #Printando somente para saber o que esta acontecendo com o codigo
+  db.commit() # "transactions are assumed, so close the transaction finished". Fecha a transacao feita, e comita as mudancas
